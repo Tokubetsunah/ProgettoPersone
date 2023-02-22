d2658e7865dd7ebecd5ce0e6c62a4613daf9260d
Tokubetsunah <73643456+Tokubetsunah@users.noreply.github.com>
2023-02-22 10:54:55 +0100

    Aggiunti commenti, pulita la funzionalita' del bottone
    Ora il bottone mostra testo "+" quando e' chiuso e "Aggiungi persona" quando e' aperto

--- app/src/main/java/com/example/progettopersone/MainActivity.kt
+++ app/src/main/java/com/example/progettopersone/MainActivity.kt
@@ -64,39 +64,46 @@ class MainActivity : AppCompatActivity() {
 
         // Verifica se la sezione di aggiunta persona è visibile e aggiorna la variabile di stato
         if (isAddPersonaVisible) {
+
+            // Recupera i valori inseriti dall'utente
+            val nome = edittext_nome.text.toString().trim()
+            val cognome = edittext_cognome.text.toString().trim()
+            val dataNascita = edittext_data_nascita.text.toString().trim()
+            val sesso = edittext_sesso.text.toString().trim()
+            val cittaNascita = edittext_citta_nascita.text.toString().trim()
+            val provinciaNascita = edittext_provincia_nascita.text.toString().trim()
+
+            // Verifica che tutti i campi siano stati inseriti
+            if (nome.isEmpty() || cognome.isEmpty() || dataNascita.isEmpty() || sesso.isEmpty() || cittaNascita.isEmpty() || provinciaNascita.isEmpty()) {
+                Toast.makeText(this, "Inserire tutti i campi", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            // Crea una nuova istanza di Persona con i valori inseriti dall'utente
+            val person = Persona(null, nome, cognome, dataNascita, sesso, cittaNascita, provinciaNascita)
+            // Aggiungi la persona al database in modo asincrono
+            GlobalScope.launch {
+                dao.insert(person)
+                showAllPeople()
+                // Cambia il testo del pulsante ad "+"
+                runOnUiThread { button_aggiungi_persona.text = "+" }
+            }
+
+            // Resetta i campi di input
+            resetInputFields()
+
+            // Mostra un messaggio di conferma
+            Toast.makeText(this, "Persona aggiunta al database", Toast.LENGTH_SHORT).show()
             hideAddPersona()
+
         } else {
+            // Cambia il testo del pulsante ad "Aggiungi persona"
+            button_aggiungi_persona.text = "Aggiungi persona"
             showAddPersona()
         }
 
-        // Recupera i valori inseriti dall'utente
-        val nome = edittext_nome.text.toString().trim()
-        val cognome = edittext_cognome.text.toString().trim()
-        val dataNascita = edittext_data_nascita.text.toString().trim()
-        val sesso = edittext_sesso.text.toString().trim()
-        val cittaNascita = edittext_citta_nascita.text.toString().trim()
-        val provinciaNascita = edittext_provincia_nascita.text.toString().trim()
-
-        // Verifica che tutti i campi siano stati inseriti
-        if (nome.isEmpty() || cognome.isEmpty() || dataNascita.isEmpty() || sesso.isEmpty() || cittaNascita.isEmpty() || provinciaNascita.isEmpty()) {
-            Toast.makeText(this, "Inserire tutti i campi", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        // Crea una nuova istanza di Persona con i valori inseriti dall'utente
-        val person = Persona(null, nome, cognome, dataNascita, sesso, cittaNascita, provinciaNascita)
-        // Aggiungi la persona al database in modo asincrono
-        GlobalScope.launch {
-            dao.insert(person)
-            showAllPeople()
-        }
-
-        // Resetta i campi di input
-        resetInputFields()
-
-        // Mostra un messaggio di conferma
-        Toast.makeText(this, "Persona aggiunta al database", Toast.LENGTH_SHORT).show()
     }
+
 
     // Metodo per mostrare la sezione di aggiunta persona
     private fun showAddPersona() {
